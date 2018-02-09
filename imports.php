dfcee709fc21859eb2191406531577aae3d3ab99
Javier-Pixart <javier.spano@pixartargentina.com.ar>
2018-02-09 10:50:37 -0300

    update

---
+++ imports.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>MDM</title>
+  <!--jQuery-->
+  <script type="text/javascript" src="libraries/jQuery/jquery-3.3.1.min.js"></script>
+  <!---------->
+
+  <!--Bootstrap-->
+  <link rel="stylesheet" type="text/css" href="libraries/Bootstrap/css/bootstrap.min.css"/>
+  <script type="text/javascript" src="libraries/Popper/popper.min.js"></script>
+  <script type="text/javascript" src="libraries/Bootstrap/js/bootstrap.min.js"></script>
+  <!------------->
+
+  <!--Chart.js-->
+  <script type="text/javascript" src="libraries/Chart.js/Chart.js"></script>
+  <!------------>
+
+  <!--DataTables-->
+  <link rel="stylesheet" type="text/css" href="libraries/DataTables/datatables.min.css"/>
+  <script type="text/javascript" src="libraries/DataTables/datatables.min.js"></script>
+  <script type="text/javascript" src="js/tables/test-table.js"></script>
+  <link rel="stylesheet" type="text/css" href="css/center.css"/>
+  <!-------------->
+
+  <!--FontAwesome-->
+  <link href="libraries/FontAwesome/web-fonts-with-css/css/fontawesome-all.min.css" rel="stylesheet">
+  <!--------------->
+
+  <!--Custom css-->
+  <link href="css/master.css" rel="stylesheet">
+  <link href="css/colors.css" rel="stylesheet">
+  <!--------------->
+</head>
+<body class="primary-bg-color">
+  
+</body>
+</html>
